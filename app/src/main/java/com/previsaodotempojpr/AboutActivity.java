3a8db819d7e0d99fddee5b71bbeb56587455c117
João Pedro Raldi <jraldi@hotmail.com>
2025-11-09 18:41:03 -0300

    adicionado botão de voltar no sobre

--- app/src/main/java/com/previsaodotempojpr/AboutActivity.java
+++ app/src/main/java/com/previsaodotempojpr/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.previsaodotempojpr;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
@@ -10,5 +13,19 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
