73f12f05eb56d96300d5ed7e172b6d64ef44ef68
João Pedro Raldi <jraldi@hotmail.com>
2025-11-09 16:46:40 -0300

    revisado e inserido fragment do maps

--- app/src/main/java/com/previsaodotempojpr/MapFragment.java
+++ app/src/main/java/com/previsaodotempojpr/MapFragment.java
@@ -9,11 +9,37 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Coordenadas de Joinville
+        LatLng joinville = new LatLng(-26.3031, -48.8456);
+        mMap.addMarker(new MarkerOptions().position(joinville).title("Marker in Joinville"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(joinville, 12f));
     }
 }
